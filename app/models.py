d0166545ead311268d390daa93d662c3c3b32edf
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-06-17 19:28:02 +0200

    feat: add models Cart and CartItem, upgrade alembic database

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, Boolean, ForeignKey, Enum, DECIMAL
+from sqlalchemy import Integer, String, Boolean, ForeignKey, Enum, DECIMAL, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from decimal import Decimal
 
@@ -14,22 +14,52 @@ class User(Base):
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
     role: Mapped[UserRole] = mapped_column(Enum(UserRole), nullable=False, default=UserRole.CUSTOMER)
 
+    # 1:1 relationship
+    cart: Mapped["Cart"] = relationship(back_populates="user", uselist=False, cascade="all, delete-orphan")
+
 class Category(Base):
     __tablename__ = "categories"
 
     category_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
 
-    products: Mapped[list[Product]] = relationship(back_populates="category")
+    products: Mapped[list["Product"]] = relationship(back_populates="category")
 
 class Product(Base):
     __tablename__ = "products"
 
     product_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    category_id: Mapped[int] = mapped_column(Integer, ForeignKey("categories.category_id"))
+    category_id: Mapped[int] = mapped_column(Integer, ForeignKey("categories.category_id"), nullable=False)
     name: Mapped[str] = mapped_column(String(30), nullable=False)
     description: Mapped[str] = mapped_column(String(200), nullable=True)
-    price: Mapped[Decimal] = mapped_column(DECIMAL, nullable=False)
+    price: Mapped[Decimal] = mapped_column(DECIMAL(10, 2), nullable=False)
     quantity: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
 
     category: Mapped[Category] = relationship(back_populates="products")
+    cart_items: Mapped[list["CartItem"]] = relationship(back_populates="product")
+
+class Cart(Base):
+    __tablename__ = "carts"
+
+    cart_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.user_id"), unique=True, nullable=False)
+
+    # cascade -> when cart gets deleted, include all its cart_items in delete
+    cart_items: Mapped[list["CartItem"]] = relationship(back_populates="cart", cascade="all, delete-orphan")
+    user: Mapped[User] = relationship(back_populates="cart")
+
+class CartItem(Base):
+    __tablename__ = "cart_items"
+
+    # makes sure there is no duplicate CartItems
+    __table_args__ = (
+        UniqueConstraint("cart_id", "product_id"),
+    )
+
+    cart_item_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    cart_id: Mapped[int] = mapped_column(Integer, ForeignKey("carts.cart_id"), nullable=False)
+    product_id: Mapped[int] = mapped_column(Integer, ForeignKey("products.product_id"), nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    cart: Mapped[Cart] = relationship(back_populates="cart_items")
+    product: Mapped[Product] = relationship(back_populates="cart_items")
